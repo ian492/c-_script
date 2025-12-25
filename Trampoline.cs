0f1f33bb53596e5a9ab5ad8f909b022b2ddbcdd9
ian <lugovaya_se@mail.ru>
2025-12-25 21:24:49 +0300

    Add files via upload

---
+++ Trampoline.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        // увеличиваем силу прыжка
+        other.GetComponent<Jump>().jumpStrength = 10;
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        // уменьшаем силу прыжка
+        other.GetComponent<Jump>().jumpStrength = 2;
+    }
+}
